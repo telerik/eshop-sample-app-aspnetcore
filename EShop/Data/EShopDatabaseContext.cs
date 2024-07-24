232ffe504c7a012ef937542ad81006ed86fe21d9
alestoya <alestoya@progress.com>
2024-07-24 13:18:23 +0300

    chore: update minimal hosting model and db context

--- EShop/Data/EShopDatabaseContext.cs
+++ EShop/Data/EShopDatabaseContext.cs
@@ -26,7 +26,8 @@ namespace Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=AspNetCoreEshop;Trusted_Connection=True;MultipleActiveResultSets=True");
+                var dataDirectory = Path.GetFullPath("wwwroot");
+                optionsBuilder.UseSqlite(@"Data Source=" + dataDirectory + System.IO.Path.DirectorySeparatorChar + @"demos.db;");
             }
         }
 
