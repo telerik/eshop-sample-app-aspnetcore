6b3a69da4adaabb7eff480fa42e817f6647f01ef
alestoya <alestoya@progress.com>
2025-04-08 11:17:13 +0300

    fix: reports are not loaded telerik/kendo#22622

---
+++ EShop/Web/Attributes/JsonConfigFilterAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Buffers;
+
+namespace Web.Attributes
+{
+    public class JsonConfigFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Result is ObjectResult objectResult)
+            {
+                var serializerSettings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Include,
+                    DateTimeZoneHandling = DateTimeZoneHandling.Unspecified,
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                var jsonOutputFormatter = new NewtonsoftJsonOutputFormatter(
+                    serializerSettings,
+                    ArrayPool<char>.Shared,
+                    new MvcOptions { },
+                    new MvcNewtonsoftJsonOptions()
+                    );
+
+                objectResult.Formatters.Add(jsonOutputFormatter);
+            }
+
+            base.OnResultExecuting(context);
+        }
+    }
+}
